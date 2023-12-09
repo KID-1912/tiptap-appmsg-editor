26bf10f135d03d07e4aa3310b0d7c7cd41e7a069
page <heyutongxue@gmail.com>
2023-12-09 18:33:50 +0800

    feat:tiptap-extension-resizable完成

--- src/extension/resizable.js
+++ src/extension/resizable.js
@@ -8,11 +8,17 @@ export default Extension.create({
       handlerStyle: {
         width: "8px",
         height: "8px",
-        background: "red",
+        background: "#07c160",
       },
       layerStyle: {
-        border: "2px solid red",
+        border: "2px solid #07c160",
       },
+    };
+  },
+
+  addStorage() {
+    return {
+      resizeElement: null,
     };
   },
 
@@ -28,6 +34,36 @@ export default Extension.create({
     Object.entries(this.options.layerStyle).forEach(([key, value]) => {
       resizeLayer.style[key] = value;
     });
+    resizeLayer.addEventListener("mousedown", (e) => {
+      const resizeElement = this.storage.resizeElement;
+      if (!resizeElement) return;
+      if (/bottom/.test(e.target.className)) {
+        let startX = e.screenX;
+        const dir = e.target.classList.contains("bottom-left") ? -1 : 1;
+        const mousemoveHandle = (e) => {
+          e.preventDefault();
+          const width = resizeElement.clientWidth;
+          const distanceX = e.screenX - startX;
+          const total = width + dir * distanceX;
+          // resizeElement
+          resizeElement.style.width = total + "px";
+          const clientWidth = resizeElement.clientWidth;
+          const clientHeight = resizeElement.clientHeight;
+          resizeElement.style.width = clientWidth + "px"; // max width
+          // resizeLayer
+          const pos = getRelativePosition(resizeElement, element);
+          resizeLayer.style.top = pos.top + "px";
+          resizeLayer.style.left = pos.left + "px";
+          resizeLayer.style.width = clientWidth + "px";
+          resizeLayer.style.height = clientHeight + "px";
+          startX = e.screenX;
+        };
+        document.addEventListener("mousemove", mousemoveHandle);
+        document.addEventListener("mouseup", () =>
+          document.removeEventListener("mousemove", mousemoveHandle)
+        );
+      }
+    });
 
     const handlers = ["top-left", "top-right", "bottom-left", "bottom-right"];
     const fragment = document.createDocumentFragment();
@@ -41,6 +77,8 @@ export default Extension.create({
       const dir = name.split("-");
       item.style[dir[0]] = parseInt(item.style.width) / -2 + "px";
       item.style[dir[1]] = parseInt(item.style.height) / -2 + "px";
+      if (name === "bottom-left") item.style.cursor = "sw-resize";
+      if (name === "bottom-right") item.style.cursor = "se-resize";
       fragment.appendChild(item);
     }
     resizeLayer.appendChild(fragment);
@@ -52,6 +90,8 @@ export default Extension.create({
     const element = editor.options.element;
     const node = transaction.curSelection.node;
     const resizeLayer = editor.resizeLayer;
+
+    console.log("selection");
     if (node && this.options.types.includes(node.type.name)) {
       // resizeLayer位置大小
       resizeLayer.style.display = "block";
@@ -59,17 +99,20 @@ export default Extension.create({
       if (dom.getAttribute("src") !== node.attrs.src) {
         dom = dom.querySelector(`[src="${node.attrs.src}"]`);
       }
+      this.storage.resizeElement = dom;
       const pos = getRelativePosition(dom, element);
       resizeLayer.style.top = pos.top + "px";
       resizeLayer.style.left = pos.left + "px";
       resizeLayer.style.width = dom.width + "px";
       resizeLayer.style.height = dom.height + "px";
     } else {
+      console.log("no node");
       resizeLayer.style.display = "none";
     }
   },
 });
 
+// 计算相对位置
 function getRelativePosition(element, ancestor) {
   const elementRect = element.getBoundingClientRect();
   const ancestorRect = ancestor.getBoundingClientRect();
