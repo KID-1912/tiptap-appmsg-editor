b55e4023a00acad74601bfdaa088f4ccf5dfeb8c
heyutongxue <heyutongxue@gmail.com>
2024-02-22 18:36:33 +0800

    fix:edator padding

--- src/js/function.js
+++ src/js/function.js
@@ -373,12 +373,12 @@ editor.on("update", () => {
 
 // 一键复制
 const $btn_copy = document.querySelector(".btn-copy");
-const clipboard = new Clipboard($btn_copy);
-clipboard.on("success", function (e) {
+const copyClipboard = new Clipboard($btn_copy);
+copyClipboard.on("success", function (e) {
   window.alert("复制成功");
   e.clearSelection(); // 清除默认选中复制内容
 });
-clipboard.on("error", function (e) {
+copyClipboard.on("error", function (e) {
   window.alert("浏览器不支持按钮复制，请手动ctrl+c");
   console.warn(e);
 });
